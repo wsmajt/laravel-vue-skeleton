fcbd6e53389502e12dec411d518b84cb938d368b
wsmajt <kacpermusz@gmail.com>
2025-03-17 20:33:54 +0100

    fixes part 2

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -13,7 +13,7 @@ import { Head } from '@inertiajs/vue3';
                 <div
                     class="flex-1 rounded-bl-lg rounded-br-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] lg:rounded-br-none lg:rounded-tl-lg lg:p-20"
                 >
-                    <h1 class="mb-1 font-medium">Let's get started</h1>
+                    <h1 class="mb-1 font-bo">Let's get started</h1>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
                         Laravel has an incredibly rich ecosystem. <br />We suggest starting with the following.
                     </p>
